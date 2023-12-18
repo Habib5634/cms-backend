b3bfca0a798a00d164a641ce39a5fd9c4e4a91c4
habib5634 <hero5276311@gmail.com>
2023-12-18 21:01:06 +0500

    jhgg

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema({
    fatherPhone:Number,
    emergencyPhone:Number,
    role:String,
-   isPasswordHidden:Boolean,
    dateOfBirth:String,
    age:Number,
    qualification:String,
